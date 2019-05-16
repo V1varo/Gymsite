dbe6c7a64a7989201d58c6b38605d3efeaf1a5d5
V1varo <50563540+V1varo@users.noreply.github.com>
2019-05-17 07:42:36 +0800

    Add files via upload

--- js/responsivenavbar.js
+++ js/responsivenavbar.js
@@ -19,7 +19,13 @@ $(document).ready(function(){
 
 //for parallax effect!!!!
 $(window).scroll(function() {
+  if ($(window).width() >= 991)
+  {
   parallax();
+}
+else{
+  mobile();
+}
   $('.toAnim').each(function() {
 
     var _win     = $(window),
@@ -38,7 +44,17 @@ function parallax() {
   var wScroll = $(window).scrollTop();
   $('.parallax').css('background-position','center '+(wScroll*0.25)+'px')
   $('.parallax2').css('background-position','50%  '+(wScroll*0.22+(590))+'px')
+  $('.parallax4').css('background-position','50%  '+(wScroll*0.22-(290))+'px')
+  $('.parallax5').css('background-position','50%  '+(wScroll*0.22+(450))+'px')
   $('.parallax3').css('background-position','50%  '+(wScroll*0.28-(400))+'px')
+}
+
+function mobile() {
+  $('.parallax').css('background-position','center')
+  $('.parallax2').css('background-position','center')
+  $('.parallax4').css('background-position','center')
+  $('.parallax5').css('background-position','center')
+  $('.parallax3').css('background-position','center')
 }
 
 
